7980c220f249d20e199a9619b987f3b119d353ae
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-07-23 22:44:51 +0300

    completed signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,20 +1,20 @@
 function Signup() {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        alert("Account created");
-    };
-    return(
-        <div className="form-container">
-            <h2>Sign Up</h2>
-            <form onSubmit={handleSubmit}>
-              <input type="text" placeholder="Full Name" required />
-              <input type="email" placeholder="Email" required />
-              <input type="password" placeholder="Password" required />
-              <button type="submit" className="button-primary">Sign Up</button>
-            </form>
-        </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Account created!");
+  };
 
-    );
-
+  return (
+    <div className="form-container">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Full Name" required />
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit" className="button-primary">Sign Up</button>
+      </form>
+    </div>
+  );
 }
+
 export default Signup;
