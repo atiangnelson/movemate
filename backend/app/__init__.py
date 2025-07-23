5464bc6aebfb9ae46871083d6fca0ce2701d01dc
Nelson <nelson.atiang@student.moringaschool.com>
2025-07-23 05:59:56 +0300

    First commit

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,18 +1,1 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-from dotenv import load_dotenv
-import os
 
-load_dotenv()  # Loads variables from .env
-
-db = SQLAlchemy()
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    CORS(app)
-
-    db.init_app(app)
-    return app
