833baa27e4ecfdf57b4ca8dbb810f821eb55125c
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-07-23 15:19:16 +0300

    added tagline text to enhance login form messaging

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,6 +2,8 @@ function Home(){
     return(
         <section className="home">
             <h1>plan your move</h1>
+            <p>Seamless moving experience starts here</p>
+            
 
         </section>
 
