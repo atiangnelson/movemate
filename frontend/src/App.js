e1628d487afb62ef14de3e560c3925c47a2e83d6
kennedy munyaga mwai <kennedy.mwai@student.moringaschool.com>
2025-07-23 12:52:32 +0300

    imported Login page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Login from "./pages/login";
 
 
 import './App.css';
