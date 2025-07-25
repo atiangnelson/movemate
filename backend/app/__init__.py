216c7111244f863789e97c07c3ac720060d6fe40
Timothy <timothy.waweru1@student.moringaschool.com>
2025-07-25 03:46:15 +0300

    initialized function app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,3 +8,8 @@ from flask_jwt_extended import JWTManager
 db = SQLAlchemy()
 jwt = JWTManager()
 
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movemate.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = "super-secret-key"
