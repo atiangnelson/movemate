135c3abca06dfc9094847e42bfc3a685f8a9b4ab
kennedy munyaga mwai <kennedy.mwai@student.moringaschool.com>
2025-07-25 10:53:30 +0300

    exported moverequestform

--- frontend/src/pages/MoveRequestForm.jsx
+++ frontend/src/pages/MoveRequestForm.jsx
@@ -2,18 +2,14 @@ function MoveRequestForm() {
     return (
         <div className="form-container">
              <h2>Move Request</h2>
-
              <form>
                 <input type="text" placeholder="From Address" required />
                 <input type="text" placeholder="To Address" required />
                 <input type="date" required />
                 <button type="submit" className="button-primary">Submit Request</button>
-
              </form>
-
-            
- 
         </div>
-    )
-
+    );
 }
+
+
