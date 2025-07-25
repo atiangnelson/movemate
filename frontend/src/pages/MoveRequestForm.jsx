4cfd40a49b33ea25fa9277b236da0bdeb00ede04
kennedy munyaga mwai <kennedy.mwai@student.moringaschool.com>
2025-07-25 10:41:33 +0300

    added a form with inputs and a submit button

--- frontend/src/pages/MoveRequestForm.jsx
+++ frontend/src/pages/MoveRequestForm.jsx
@@ -2,7 +2,13 @@ function MoveRequestForm() {
     return (
         <div className="form-container">
              <h2>Move Request</h2>
-             <form></form>
+             <form>
+                <input type="text" placeholder="From Address" required />
+                <input type="text" placeholder="To Address" required />
+                <input type="date" required />
+                <button type="submit" className="button-primary">Submit Request</button>
+
+             </form>
         </div>
     )
 
