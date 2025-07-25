77e08e60a90d13ddfca8be7f7a3d910a9c41d0b0
christopher <vice7440@gmail.com>
2025-07-25 08:12:42 +0300

    change prof jsx email

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -2,8 +2,8 @@ function Profile() {
   return (
     <div className="page-container">
       <h2>User Profile</h2>
-      <p>Name: John Doe</p>
-      <p>Email: johndoe@example.com</p>
+      <p>Name: Move Mate</p>
+      <p>Email: movemate@gmail.com</p>
     </div>
   );
 }
