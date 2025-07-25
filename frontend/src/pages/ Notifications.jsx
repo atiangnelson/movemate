942f7d00bd2b735fda48c1ddaf166e021fd8ec88
kennedy munyaga mwai <kennedy.mwai@student.moringaschool.com>
2025-07-25 07:36:53 +0300

    exported notifications

--- frontend/src/pages/ Notifications.jsx
+++ frontend/src/pages/ Notifications.jsx
@@ -1,14 +1,14 @@
 function Notifications() {
     return (
         <div className="page-container">
-            <h2>Notifications</h2>
-            <ul>
-        <li>Your move request is being reviewed.</li>
-        <li>Your quote has been approved.</li>
-      </ul>
-             
-
+          <h2>Notifications</h2>
+          <ul>
+            <li>Your move request is being reviewed.</li>
+            <li>Your quote has been approved.</li>
+          </ul>
         </div>
-    )
+    );
 
 }
+
+export default Notifications;
