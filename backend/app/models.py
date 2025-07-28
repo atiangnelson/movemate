d8a3c73b9dfbf0c0dc7779fd1ba9f766292f9b9e
Timothy <timothy.waweru1@student.moringaschool.com>
2025-07-28 10:43:12 +0300

    created instances id and user_id

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,3 +5,5 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(200))
 class MoveRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
