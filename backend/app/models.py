736009900e25645490a46012b10943d5375eb172
Timothy <timothy.waweru1@student.moringaschool.com>
2025-07-28 11:32:00 +0300

    removed commment

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).parent.parent))  # Adjust if needed
+sys.path.append(str(Path(__file__).parent.parent))  
 
 
 from app import db
